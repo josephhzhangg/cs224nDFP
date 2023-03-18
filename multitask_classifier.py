aee50587502f03387fe8b04845ee9cef7ed083b2
alexjpeng <alexander.j.peng@gmail.com>
2023-03-17 17:27:18 -0700

    cosine sim v1?

--- multitask_classifier.py
+++ multitask_classifier.py
@@ -74,7 +74,7 @@ class MultitaskBERT(nn.Module):
         # also show difference between cosine similarity and linear layer
         self.similarity_predicter = nn.Linear(2*BERT_HIDDEN_SIZE, 1)
         # cosine similarity
-        # self.cosine_similarity = nn.CosineSimilarity(dim=1, eps=1e-6)
+        self.cosine_similarity = nn.CosineSimilarity(dim=1, eps=1e-6)
         self.dropout = nn.Dropout(config.hidden_dropout_prob)
 
     def forward(self, input_ids, attention_mask):
@@ -136,12 +136,15 @@ class MultitaskBERT(nn.Module):
         # cls_output_2 = F.normalize(cls_output_2)
         # logits = (cls_output_1 * cls_output_2).sum(dim=1)
 
-        logits = self.similarity_predicter(
-            torch.cat((outputs_1, outputs_2), dim=1)
-        )
-        # rescale from -1 to 1 to 0 to 5
-        # multiply by 2.5 and add 2.5
-        logits = logits * 2.5 + 2.5
+        # logits = self.similarity_predicter(
+        #     torch.cat((outputs_1, outputs_2), dim=1)
+        # )
+        #use cosine similarity
+        logits = self.cosine_similarity(outputs_1, outputs_2)
+        #cosine similarity outputs a value between -1 and 1
+        #we want to output a class between 0 and 5
+        #so we need to scale the output
+        logits = (logits + 1) * 2.5
         return logits
 
 # optimizer._optimizer.state_dict() for gradient surgery
